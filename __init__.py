ea4a0ae7a3c595a3c1128d950330fadf7d2d0be4
nahjh2 <tkfkadls123>
2019-07-15 10:56:03 +0900

    that was mistake

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-__import__('hackathon.database')
-__import__('hackathon.resource')
+__import__('itIsTasty.database')
+__import__('itIsTasty.resource')
