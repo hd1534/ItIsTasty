78b96055f0ec89d56539c4e4c63e73608ecde90a
nahjh2 <tkfkadls123>
2019-07-15 11:41:59 +0900

    that was mistake

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-__import__('itIsTasty.database')
-__import__('itIsTasty.resource')
+__import__('ItIsTasty.database')
+__import__('ItIsTasty.resource')
--- loader.py
+++ loader.py
@@ -1,8 +1,8 @@
 def resource_load(*resources):
     for resource in resources:
-        __import__('hackathon.resource.{}'.format(resource))
+        __import__('ItIsTasty.resource.{}'.format(resource))
 
 
 def model_load(*models):
     for model in models:
-        __import__('hackathon.database.{}'.format(model))
+        __import__('ItIsTasty.database.{}'.format(model))
