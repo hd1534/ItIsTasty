eb3b5528cd29b14d5d3403a7f0a42d699510cb84
nahjh2 <tkfkadls123>
2019-07-19 16:52:50 +0900

    add mission

--- database/mission.py
+++ database/mission.py
@@ -20,3 +20,48 @@ class Mission(db.Model):
     #log = db.relationship("Log", back_populates="mission")
 
 
+def add_mission(data):
+    db.session.add(Mission(
+        title=data['title'],
+        description=data['description'],
+        start_time=data['start_time'],
+        end_time=data['end_time'],
+        reward=data['reward']
+    ))
+    db.session.commit()
+
+
+def update_mission(id, data):
+    mission = Mission.query.filter_by(id=id)
+
+    if mission.first() is None:
+        return 404
+
+    mission.update({
+        'title': data['title'],
+        'description': data['description'],
+        'start_time': data['start_time'],
+        'end_time': data['end_time'],
+        'reward': data['reward']
+    })
+    db.session.commit()
+    return 200
+
+
+def get_mission(id):
+    return Mission.query.filter_by(id=id).first
+
+
+def get_all_mission():
+    return Mission.query.all()
+
+
+def delete_mission(id):
+    mission = Mission.query.filter_by(id=id).first()
+    if mission is None:
+        return 404
+    db.session.delete(mission)
+    db.session.commit()
+    return 200
+
+
---
+++ resource/mission.py
@@ -0,0 +1,66 @@
+from ItIsTasty.resource import api
+from flask_restplus import Resource, fields, marshal
+from flask import request, send_file
+
+from ItIsTasty.database.mission import(
+    add_mission,
+    update_mission,
+    get_mission,
+    get_all_mission,
+    delete_mission
+)
+
+
+ns = api.namespace('mission', description='미션 정보')
+
+
+mission_model = ns.model('MissionModel', {
+    'title': fields.String(required=True),
+    'description': fields.String(required=True),
+    'start_time': fields.DateTime(required=True),
+    'end_time': fields.DateTime(required=True),
+    'reward': fields.Integer(required=False)
+})
+
+full_mission_model = ns.model('MissionListModel', {
+    'idx': fields.Integer(required=True),
+    'title': fields.String(required=True),
+    'description': fields.String(required=True),
+    'start_time': fields.DateTime(required=True),
+    'end_time': fields.DateTime(required=True),
+    'reward': fields.Integer(required=False)
+})
+
+mission_list_model = ns.model('MissionListModel', {
+    'users': fields.List(fields.Nested(full_mission_model))
+})
+
+
+@ns.route('/')
+class UserResource(Resource):
+    @ns.expect(mission_model)
+    @ns.doc(responses={200: '성공'},
+            description='''미션을 추가합니다.''')
+    def post(self):
+        add_mission(request.get_json())
+        return {}, 200
+
+    @ns.marshal_with(mission_list_model)
+    @ns.doc(description='''모든 미션을 출력합니다.''',
+            responses={200: '성공'})
+    def get(self):
+        return {'users': get_all_mission()}
+
+
+@ns.route('/<int:mission_idx>')
+class UserIdxResource(Resource):
+    @ns.expect(mission_model)
+    @ns.doc(responses={200: '성공', 404: '없는 미션입니다.'},
+            description='''미션 정보를 수정합니다.''')
+    def put(self, mission_idx):
+        return {}, update_mission(mission_idx, request.get_json())
+
+    @ns.doc(responses={200: '성공', 404: '없는 미션입니다.'},
+            description='''미션을 삭제합니다.''')
+    def delete(self, mission_idx):
+        return {}, delete_mission(mission_idx)
